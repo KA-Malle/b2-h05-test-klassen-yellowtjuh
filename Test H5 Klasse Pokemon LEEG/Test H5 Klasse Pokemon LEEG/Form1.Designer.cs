6359bb79d62170bd1bf3e67890f780bd5304efae
Jelle van Dummelen <jelle.van.dummelen@telenet.be>
2025-04-24 11:07:13 +0200

    Test klassen

--- Test H5 Klasse Pokemon LEEG/Test H5 Klasse Pokemon LEEG/Form1.Designer.cs
+++ Test H5 Klasse Pokemon LEEG/Test H5 Klasse Pokemon LEEG/Form1.Designer.cs
@@ -43,9 +43,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(59, 26);
+            this.label1.Location = new System.Drawing.Point(88, 41);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(128, 32);
+            this.label1.Size = new System.Drawing.Size(192, 52);
             this.label1.TabIndex = 0;
             this.label1.Text = "Speler 1";
             // 
@@ -53,80 +54,93 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(349, 26);
+            this.label3.Location = new System.Drawing.Point(524, 41);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(128, 32);
+            this.label3.Size = new System.Drawing.Size(192, 52);
             this.label3.TabIndex = 2;
             this.label3.Text = "Speler 2";
             // 
             // buttonValAan1
             // 
-            this.buttonValAan1.Location = new System.Drawing.Point(65, 108);
+            this.buttonValAan1.Location = new System.Drawing.Point(98, 169);
+            this.buttonValAan1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonValAan1.Name = "buttonValAan1";
-            this.buttonValAan1.Size = new System.Drawing.Size(154, 23);
+            this.buttonValAan1.Size = new System.Drawing.Size(231, 36);
             this.buttonValAan1.TabIndex = 3;
             this.buttonValAan1.Text = "Val speler 2 aan...";
             this.buttonValAan1.UseVisualStyleBackColor = true;
+            this.buttonValAan1.Click += new System.EventHandler(this.buttonValAan1_Click);
             // 
             // buttonValAan2
             // 
-            this.buttonValAan2.Location = new System.Drawing.Point(341, 108);
+            this.buttonValAan2.Location = new System.Drawing.Point(512, 169);
+            this.buttonValAan2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonValAan2.Name = "buttonValAan2";
-            this.buttonValAan2.Size = new System.Drawing.Size(154, 23);
+            this.buttonValAan2.Size = new System.Drawing.Size(231, 36);
             this.buttonValAan2.TabIndex = 4;
             this.buttonValAan2.Text = "Val speler 1 aan...";
             this.buttonValAan2.UseVisualStyleBackColor = true;
+            this.buttonValAan2.Click += new System.EventHandler(this.buttonValAan2_Click);
             // 
             // labelGezondheid1
             // 
             this.labelGezondheid1.AutoSize = true;
-            this.labelGezondheid1.Location = new System.Drawing.Point(62, 75);
+            this.labelGezondheid1.Location = new System.Drawing.Point(93, 117);
+            this.labelGezondheid1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelGezondheid1.Name = "labelGezondheid1";
-            this.labelGezondheid1.Size = new System.Drawing.Size(86, 16);
+            this.labelGezondheid1.Size = new System.Drawing.Size(140, 25);
             this.labelGezondheid1.TabIndex = 5;
             this.labelGezondheid1.Text = "Gezondheid: ";
             // 
             // labelGezondheid2
             // 
             this.labelGezondheid2.AutoSize = true;
-            this.labelGezondheid2.Location = new System.Drawing.Point(352, 75);
+            this.labelGezondheid2.Location = new System.Drawing.Point(528, 117);
+            this.labelGezondheid2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelGezondheid2.Name = "labelGezondheid2";
-            this.labelGezondheid2.Size = new System.Drawing.Size(86, 16);
+            this.labelGezondheid2.Size = new System.Drawing.Size(140, 25);
             this.labelGezondheid2.TabIndex = 6;
             this.labelGezondheid2.Text = "Gezondheid: ";
             // 
             // buttonGenees1
             // 
-            this.buttonGenees1.Location = new System.Drawing.Point(65, 148);
+            this.buttonGenees1.Location = new System.Drawing.Point(98, 231);
+            this.buttonGenees1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonGenees1.Name = "buttonGenees1";
-            this.buttonGenees1.Size = new System.Drawing.Size(154, 23);
+            this.buttonGenees1.Size = new System.Drawing.Size(231, 36);
             this.buttonGenees1.TabIndex = 7;
             this.buttonGenees1.Text = "Genees";
             this.buttonGenees1.UseVisualStyleBackColor = true;
+            this.buttonGenees1.Click += new System.EventHandler(this.buttonGenees1_Click);
             // 
             // buttonGenees2
             // 
-            this.buttonGenees2.Location = new System.Drawing.Point(341, 148);
+            this.buttonGenees2.Location = new System.Drawing.Point(512, 231);
+            this.buttonGenees2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonGenees2.Name = "buttonGenees2";
-            this.buttonGenees2.Size = new System.Drawing.Size(154, 23);
+            this.buttonGenees2.Size = new System.Drawing.Size(231, 36);
             this.buttonGenees2.TabIndex = 8;
             this.buttonGenees2.Text = "Genees";
             this.buttonGenees2.UseVisualStyleBackColor = true;
+            this.buttonGenees2.Click += new System.EventHandler(this.buttonGenees2_Click);
             // 
             // buttonToonPos
             // 
-            this.buttonToonPos.Location = new System.Drawing.Point(203, 198);
+            this.buttonToonPos.Location = new System.Drawing.Point(304, 309);
+            this.buttonToonPos.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonToonPos.Name = "buttonToonPos";
-            this.buttonToonPos.Size = new System.Drawing.Size(175, 23);
+            this.buttonToonPos.Size = new System.Drawing.Size(262, 36);
             this.buttonToonPos.TabIndex = 9;
             this.buttonToonPos.Text = "Toon posities";
             this.buttonToonPos.UseVisualStyleBackColor = true;
+            this.buttonToonPos.Click += new System.EventHandler(this.buttonToonPos_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(556, 242);
+            this.ClientSize = new System.Drawing.Size(834, 378);
             this.Controls.Add(this.buttonToonPos);
             this.Controls.Add(this.buttonGenees2);
             this.Controls.Add(this.buttonGenees1);
@@ -136,8 +150,10 @@
             this.Controls.Add(this.buttonValAan1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Pokemon game";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
